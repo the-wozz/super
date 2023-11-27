3743e6a092544b76033542c997a0b67545e5f119
Woz <wozzy4@gmail.com>
2023-11-27 12:07:24 -0700

    Update super_logs_upload.sh
    minor spelling fixes and also added some checks for Jamf Pro url detection

--- super_logs_upload.sh
+++ super_logs_upload.sh
@@ -1,55 +1,73 @@
 #!/bin/bash
 
 # What it do: Gathers SUPER log files for automatic upload to Jamf Pro Computer record for reviewing issues
+# ONLY works for SUPER 4
+
+# Author:
 # Zachary 'Woz'nicki & Brian Oanes
-# Oirginal: 3/14/23 
+# OG: 3/14/23 
 # Revision 2: 5/25/23
 # Revision 3: 11/22/23
 # Revision 4: 11/27/23
 
 # How to use:
-# Set lines 20 AND 22 
+# Set Paramter 4 [Client ID] and Parmeter 5 [Client Secret] in Script Parameters within policy OR change line 24 AND 26 from '$4' and '$5' to Client ID and Client Secret [respectively] contained within quotes to bypass Script Parameters
+# ONLY set 'jssURL' if you want to bypass the automatic checking for a Jamf Pro plist URL on the machine
 
 ## JSS Information ##
-# Manually set JSS address
-#jssURL=""
+# Manually set JSS address to bypass automatic detection
+jssURL=""
 
 ## API Clients and Roles ##
-# Read AND Update Computers privileges required for script to work
-## Input $4 - API Client ID
+### READ ME: Read Computers AND Update Computers privileges required for script to work! ###
+## Input 4 in 'Script Parameters' - API Client ID
 clientID=$4
-## Input $5 - API Secret
+## Input 5 in 'Script Paramters' - API Client Secret
 clientSecret=$5
 
 ## Global Variables ##
+# Serial number
 machineSerial=$(system_profiler SPHardwareDataType | grep Serial |  awk '{print $NF}' )
-#currentUser=$(who | awk '/console/{print $1}')
+# Main SUPER folder
+superFolder="/Library/Management/super"
+# Bearer Token leave blank
 bearerToken=""
-jamfProID=""
 
 ## Functions ##
 checkPlist() {
+    if [[ -z "$jssURL" ]]; then
+    echo "Looking for JSS..."
 plistCheck=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist | plutil -extract jss_url raw -| rev | cut -c 2- | rev)
-    if [[ -n "$plistCheck" ]]; then
-        echo "JAMF Pro Plist found!"
-        jssURL="$plistCheck"
+        if [[ -n "$plistCheck" ]]; then
+            echo "Jamf Pro plist found!"
+            jssURL="$plistCheck"
+            echo "JSS: $jssURL"
+        else
+            echo "No Jamf Pro plist found. Exiting..."
+            exit 1
+        fi
+    else
+        echo "Manually entered JSS: $jssURL"
+    fi
+}
+
+superCheck() {
+    if [ -z "$(ls -A $superFolder)" ]; then
+        echo "SUPER folder does NOT exist. Exiting..."
+        exit 1
     fi
 }
 
 getBearerToken() {
-    echo "JSS: $jssURL"
     response=$(curl -s -L -X POST "$jssURL"/api/oauth/token \
                     -H 'Content-Type: application/x-www-form-urlencoded' \
                     --data-urlencode "client_id=$clientID" \
                     --data-urlencode 'grant_type=client_credentials' \
                     --data-urlencode "client_secret=$clientSecret")
     bearerToken=$(echo "$response" | plutil -extract access_token raw -)
-    #echo "Bearer Token: $bearerToken"
 }
 
 uploadAttachment() {
-# Main SUPER folder
-superFolder="/Library/Management/super"
 # SUPER logs folder
 superLogFolder="/Library/Management/super/logs"
 # SUPER plist 
@@ -59,20 +77,14 @@ current_time=$(date "+%m-%d-%Y-%H-%M")
 # Zipped file name with machine serial and date for better understanding
 new_fileName="super-$machineSerial-$current_time"
 
-# Check if SUPER folder is not empty and then zip the files together
-    if [ -z "$(ls -A $superFolder)" ]; then
-        echo "SUPER folder does NOT exist. Exiting..."
-        exit 0
-    else
-        zip -j "/private/tmp/$new_fileName.zip" "$superLogFolder"/*.log "$superPlist"
-    fi
+zip -j "/private/tmp/$new_fileName.zip" "$superLogFolder"/*.log "$superPlist"
 
-    # Get Jamf Pro ID for machine
-    jamfProID=$(curl -H "accept: application/xml" -H "Authorization: Bearer ${bearerToken}" \
+# Get Jamf Pro ID for machine via serial number variable
+jamfProID=$(curl -H "accept: application/xml" -H "Authorization: Bearer ${bearerToken}" \
     -X GET "${jssURL}/JSSResource/computers/serialnumber/${machineSerial}/subset/general" | awk -F '<id>' '{print $2}' | awk -F '<' '{print $1}' )
     
-    # Upload attachment to Computer record via Jamf Pro ID
-    curl -X POST "${jssURL}/api/v1/computers-inventory/${jamfProID}/attachments" \
+# Upload attachment to Computer record via Jamf Pro ID
+curl -X POST "${jssURL}/api/v1/computers-inventory/${jamfProID}/attachments" \
     -H "Authorization: Bearer ${bearerToken}" \
     -F file=@/private/tmp/"$new_fileName".zip
      
@@ -92,12 +104,14 @@ invalidateToken() {
 
 ## Main Body ##
 checkPlist
+superCheck
 getBearerToken
 uploadAttachment
 
 ## Cleanup ##
 rm "/private/tmp/$new_fileName.zip"
-    echo "Super log zip file removed"
+echo "Super log zip file removed"
+
 invalidateToken
 
 exit 0
